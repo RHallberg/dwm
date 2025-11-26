f56ea233e15ad2269d9e626dd2fab28ca1d5084d
RHallberg <hallbergrickard@hotmail.com>
2025-11-26 18:17:10 +0100

    Let dmenu use its default colors

--- config.h
+++ config.h
@@ -97,7 +97,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 static const char *termcmd[]  = { "ghostty", NULL };
 static const char *browsercmd[]  = { "zen-browser", NULL };
 static const char *emojipickercmd[] = { "emoji_picker", NULL};
